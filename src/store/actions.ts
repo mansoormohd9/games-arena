9ebcdec97b5758f33e070c080332b81797eb7faa
GREENWICH\smohammad <shaikmansoor.mohammad@factset.com>
2020-02-23 14:23:19 +0530

    REading data from json as cors has not been enabled on api shared

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,10 +1,13 @@
 import {ActionTree, ActionContext} from 'vuex';
 import {AppState} from '../types/types';
 import * as api from "../utils/api/index";
+import gamesData from "../utils/games-data.json";
 
 const actions: ActionTree<AppState, AppState> = {
   async initialize({commit}: ActionContext<AppState, AppState>) {
-    const games = await api.getGamesData();
+    //commenting out the below line of code as it seems like cors has not been enabled on the api shared in documentation so reading the data from json file
+    //const games = await api.getGamesData();
+    const games = gamesData;
     commit("setGames", games);
   }
 };
