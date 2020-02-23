b9c07f1989f68b147124d6209e7192c9f07ce230
GREENWICH\smohammad <shaikmansoor.mohammad@factset.com>
2020-02-23 13:34:58 +0530

    Adding config for heroku deployment

--- src/utils/api/methods.ts
+++ src/utils/api/methods.ts
@@ -1,7 +1,13 @@
 import { Vue } from "vue-property-decorator";
 
 export function getJson(url: string) {
-  return Vue.axios.get(url).then((response) => {
+  return Vue.axios.get(url, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'accept': 'application/json',
+    },
+    data: {}
+    }).then((response) => {
     console.log(response.data);
     return response.data;
   })
